1d76b60665505b5759e4e031ff9926f2d44a42d3
Russell Duhon <fugu13@gmail.com>
2012-10-17 10:00:43 -0700

    keep moving, now with overlays

--- verbs.js
+++ verbs.js
@@ -2,12 +2,14 @@ requirejs.config({
     shim: {
         'lib/underscore-min.js': {
             exports: '_'
-        }
+        },
+        'http://cdn.jquerytools.org/1.2.7/all/jquery.tools.min.js': ['jquery']
     },
     baseUrl: 'verbs'
 });
 
-require(["jquery", "lib/underscore-min.js", "list.js", "lib/domReady.js"], function($, _, verbs) {
+
+require(["jquery", "lib/underscore-min.js", "list.js", 'http://cdn.jquerytools.org/1.2.7/all/jquery.tools.min.js', "lib/domReady.js"], function($, _, verbs) {
     _.templateSettings = {
         interpolate : /\{\{(.+?)\}\}/g,
         escape: /\{\{-(.+?)\}\}/g,
@@ -16,8 +18,11 @@ require(["jquery", "lib/underscore-min.js", "list.js", "lib/domReady.js"], funct
     var template = _.template($('#verb-template').html());
     var container = $('#container');
     $(function() {
+        var counter = 0;
         _.each(verbs.modules, function(verb) {
+            verb.hidden = 'hidden' + counter++;
             $(template(verb)).appendTo(container);
         });
+        $("div[rel]").overlay();
     });
 });
