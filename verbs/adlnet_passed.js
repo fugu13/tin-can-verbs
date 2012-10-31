109e48409e1df3e4567785e01fee77ea71196d68
Russell Duhon <fugu13@gmail.com>
2012-10-31 10:45:18 -0700

    lots more verbs

---
+++ verbs/adlnet_passed.js
@@ -0,0 +1,7 @@
+define({
+    uri: 'http://adlnet.gov/expapi/verbs/passed',
+    description: "Completing an activity satisfactorily, such as exceeding a minimum threshold. Examples include: passing a foreign language quiz.",
+    display: {
+        'en': 'passed'
+    }
+});
