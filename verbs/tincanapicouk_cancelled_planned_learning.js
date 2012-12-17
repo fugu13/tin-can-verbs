55be6697556f4c4d834a31ac6f83a9ff52f7dab5
Andrew Downes <mrdownes@hotmail.com>
2012-12-17 19:57:48 +0000

    added verbs from tincanapi.co.uk
    See http://www.tincanapi.co.uk/wiki/Verbs

---
+++ verbs/tincanapicouk_cancelled_planned_learning.js
@@ -0,0 +1,7 @@
+define({
+    uri: 'http://www.tincanapi.co.uk/wiki/verbs:cancelled_planned_learning',
+    description: 'Part of the Learning Plan Framework. Object should be an StatementRef.',
+    display: {
+        'en': 'cancelled'
+    }
+});
---
+++ verbs/tincanapicouk_evaluated.js
@@ -0,0 +1,7 @@
+define({
+    uri: 'http://www.tincanapi.co.uk/wiki/verbs:evaluated',
+    description: "Used for evaluating a learning activity. Object should be a StatementRef.",
+    display: {
+        'en': 'evaluated'
+    }
+});
